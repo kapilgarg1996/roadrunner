62d4147e4ad3c4ba5534372392adfb32fb59d868
kapil33 <kapilphalswal3@gmail.com>
2016-04-03 13:05:23 +0530

    port-changes-done

--- runner/views.py
+++ runner/views.py
@@ -100,11 +100,6 @@ def login_form(request):
 def test(request):
     return render(request, 'runner/test.html')
 
-def route_form(request):
-    form = forms.RouteForm()
-
-    return render(request, 'runner/routeform.html', {'form':form})
-
 def user_signup(request):
     if request.method=='POST':
         form = forms.UserSignupForm(request.POST, request.FILES)
@@ -124,38 +119,8 @@ def user_signup(request):
 
 
 @api_view(['GET'],)
-def get_stops(request):
-    cursor = connection.cursor()
-    sql = """select Stop.ID as id, Stop.NAME as name, Stop.CITY as city from Stop where Stop.ID != -1"""
-    cursor.execute(sql)
-    queryset = dictfetchall(cursor)
-    #queryset = Stop.objects.raw(sql)
-    result = StopSerializer(queryset, many=True)
-    return Response(result.data)
 
-@api_view(['POST', 'GET'],)
-def get_routes(request):
-    source = request.POST['source']
-    dest = request.POST['dest']
-    time = request.POST['time']
-    now = '2016-02-10 17:00:00'
-    sql = ROUTE_SEARCH_QUERY
-    queryset = Route.objects.raw(sql, (source, dest, now, time, time) )
-    result = RouteSerializer(queryset, many=True)
-    return Response(result.data)
 
-@api_view(['GET'])
-def get_route_detail(request, id):
-    cursor = connection.cursor()
-    rid = id
-    sql = ROUTE_DETAIL_QUERY
-    cursor.execute(sql, (rid,))
-    queryset = dictfetchall(cursor)
-    #nqueryset = lower_keys(queryset)
-    result = RouteDetailSerializer(queryset, many=True)
-    return Response(result.data)
-
-@api_view(['GET'])
 def get_user_detail(request, id):
     cursor = connection.cursor()
     uid = id
