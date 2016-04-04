c709b7633fd709419f1caff5d8a1062108944300
kapil garg <kapilgarg1996@gmail.com>
2016-04-04 22:57:53 +0530

    models confirm

--- taxi/urls.py
+++ taxi/urls.py
@@ -4,6 +4,7 @@ from taxi import views
 
 urlpatterns = [
         url(r'^taxis/', views.get_taxis, name='all_available_taxis'),
+        url(r'^places', views.get_places, name='all_places'),
         url(r'taxi-detail/', views.taxi_detail, name='taxi_detail'),
         url(r'taxi-choice/', views.taxi_choice, name='taxi_by_choice'),
         url(r'book-taxi/', views.book_taxi, name='book_taxi'),
